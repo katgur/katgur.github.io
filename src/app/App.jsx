0073e41c0ec084c7bc119cdf04cb99239de364ba
Kate <mitsutanaka29@gmail.com>
2023-03-12 22:54:30 +0300

    - Configured for several project

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,15 +5,15 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { Route, Routes } from 'react-router-dom'
 
-export const App = () => {
+export const App = ({ link }) => {
     return (
       <DndProvider backend={HTML5Backend}>
         <Routes>
-          <Route path='/' element={<MainPage />}>
-            <Route path='create' element={<TaskPage />} />
-            <Route path='edit/:id' element={<TaskPage />} />
+          <Route path={link} element={<MainPage />}>
+            <Route path={`${link}/create`} element={<TaskPage />} />
+            <Route path={`${link}/edit/:id`} element={<TaskPage />} />
           </Route>
-          <Route path='full/:id' element={<FullPage />} />
+          <Route path={`${link}/full/:id`} element={<FullPage />} />
         </Routes>
       </DndProvider>
       )
