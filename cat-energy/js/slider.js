b1a575af3fbe8ec1eb14f1d35d6f6b30d099913f
Kate <mitsutanaka29@gmail.com>
2023-11-25 21:59:55 +0300

    Updates

--- cat-energy/js/slider.js
+++ cat-energy/js/slider.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var input = document.getElementsByClassName('before-after__slider-input')[0];
     var beforeButton = document.getElementsByClassName('before-after__text_type_toggle--before')[0];
     var afterButton = document.getElementsByClassName('before-after__text_type_toggle--after')[0];
+
     input.addEventListener("input", (e => {
         slider.style.gridTemplateColumns = "1fr " + (100 - e.target.value) + "%"
     }));
@@ -14,4 +15,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
         input.value = 100;
         slider.style.gridTemplateColumns = "1fr 0%"
     })
+
+    var onMobileSize = window.matchMedia('(max-width: 699px)');
+    function handleMobileSize(e) {
+        if (e.matches) {
+            input.step = 100;
+            input.value = 0;
+            slider.style.gridTemplateColumns = "1fr 100%";
+        }
+    }
+    onMobileSize.addListener(handleMobileSize);
+    handleMobileSize(onMobileSize);
+
+    var onTabletSize = window.matchMedia('(min-width: 700px)');
+    function handleTabletSize(e) {
+        if (e.matches) {
+            input.step = 1;
+            input.value = 50;
+            slider.style.gridTemplateColumns = "1fr 50%";
+        }
+    }
+    onTabletSize.addListener(handleTabletSize);
+    handleTabletChange(onTabletSize);
 });
