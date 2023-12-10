a41f862db57e2cd10442b470ee54ab8db4d1cb10
katgur <mitsutanaka29@gmail.com>
2023-12-10 16:53:28 +0300

    Updates

--- little-lemon/js/animate.js
+++ little-lemon/js/animate.js
@@ -12,4 +12,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
             behavior: "smooth",
         });
     })
+    
+    const inViewport = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animation__scale-in");
+            } else {
+                entry.target.classList.remove("animation__scale-in");
+            }
+        });
+    };
+
+    const animated = document.getElementsByClassName('animation');
+    Array.prototype.forEach.call(animated, el => {
+        const Obs = new IntersectionObserver(inViewport);
+        const obsOptions = {};
+
+        Obs.observe(el, obsOptions);
+    })
 });
